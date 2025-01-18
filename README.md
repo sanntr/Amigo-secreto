586128980c01045f9c34a70918b06e95e9f0faab
sanntr <sanntrv@gmail.com>
2025-01-17 20:24:44 -0500

    creacion del readme

---
+++ README.md
@@ -0,0 +1,38 @@
+<div align="center">
+<h1> Juego amigo secreto </h1>
+<img  src="imgReadme\logo.png"> 
+
+En este desafío, desarrollarás una aplicación que permita a los usuarios ingresar nombres de amigos en una lista para luego realizar un sorteo aleatorio y determinar quién es el "amigo secreto"
+</div>
+
+## Proposito del proyecto
+Poner en practica la logica de programacion, el manejo de java script en el desarrollo de una serie de requerimientos y la implementacion de git como repositorio.
+### Funcionamiento
+El usuraio puede ingresar  tantos nombres como deseé siempre que cumpla con las siguientes condiciones: 
+
+* Minimo 3 caracteres
+
+* No acepta valores nulos
+
+* No puede contener numeros
+
+* No se puede repetir nombres (usar la inicial de apellido despues del nombre)
+
+Si no se cumplen estas condiciones encima del input saldra un mensaje diciendo ingrese un nombre valido.
+
+![imagen dato de entrada no valido](imgReadme\img-novalido.png)
+
+En el caso contrario debajo del input saldra la lista de nombres.
+
+![imagen valida](imgReadme\img-valido.png)
+
+Cuando esten todos los nombres se realizara el sorte al oprimir el boton "sortear amigo", se mostrara el nombre escogido alazar.
+
+![imagen sortear amigo](imgReadme\sortear-amigo.png)
+---
+
+## Informacion adicional
+
+No importa si se ingresa  un nombre con mayusculas el programa las cambiara a minusculas y cualquier espacio en blanco sera eliminado.
+Aunque el proyecto es funcional tiene mejoras que se pueden realiza: distinguir entre nombres y letras alazar y no aceptar simbolos 
+
--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos =[]; 
 let listaIndex=[];
-const listaNumeros=[0,1,2,4,5,6,7,8,9];
+let  indexValo=0;
+const listaNumeros=[0,1,2,4,5,6,7,8,9];// Lista de numeros para validar
 
 function asignarTextoElemento(elemento, texto){
     let etiqueta= document.querySelector(elemento)
     etiqueta.innerHTML=texto
 };
 
-
 function agregarAmigo(){
     //Estable la entrada de dato en minuscula y elimina los espacios vacios
     datoEntrada= document.getElementById('amigo').value.toLowerCase().trim();
 
     //validacion por consola
-    console.log(listaAmigos.includes(datoEntrada));
+    // console.log(listaAmigos.includes(datoEntrada));
 
     //aseguutarses de que el string no sea numeros
     for(i of datoEntrada){
         // console.log(i)
         if(i in listaNumeros ){
             return asignarTextoElemento('#descripcion', "Ingrese un nombre valido"); 
-            
         }
     }
 
@@ -38,15 +37,12 @@ function agregarAmigo(){
     //se limpia el input y muestra la lista de amigos
     document.querySelector('#amigo').value=''
     asignarTextoElemento('#listaAmigos', listaAmigos);
-
-    console.log(listaAmigos);
+    // console.log(listaAmigos);
 }
 
 
 function sortearAmigo(){
-
-    let  indexValor=Math.floor(Math.random()*listaAmigos.length);
-    
+    indexValor=Math.floor(Math.random()*listaAmigos.length);
     //valida que todos los nombres se sortearon 
     if  (listaIndex.length===listaAmigos.length)    {
         asignarTextoElemento('h2', "Todos los nombres fueron sorteados");
@@ -56,7 +52,7 @@ function sortearAmigo(){
                 sortearAmigo();
         }else   {
                 listaIndex.push(indexValor);
-                asignarTextoElemento('h2', listaAmigos[indexValor]);
+                asignarTextoElemento('h2', listaAmigos[indexValor] );
             }
     }
 }
